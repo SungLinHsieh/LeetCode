316b51d717fc15cfb9fe261279bafde9fddbbfb6
SungLinHsieh <r02323007@ntu.edu.tw>
2017-02-11 03:23:58 -0500

    revise 1 to O(n)

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,9 +1,15 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        m = min(nums)
+        l = [False]*(target+1-2*m)
+        n = [-1]*(target+1-2*m)
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
+            if nums[i]<=target-m:
+                if l[target - nums[i]-m]==True:
+                    return[n[target - nums[i]-m] ,i]
+                else:
+                    l[nums[i]-m]=True
+                    n[nums[i]-m]=i
         """
         :type nums: List[int]
         :type target: int
