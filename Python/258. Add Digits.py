a8079295389114eebf928041e9d502cc18b2eecd
SungLinHsieh <r02323007@ntu.edu.tw>
2017-02-13 18:50:19 -0500

    upload 258

---
+++ Python/258. Add Digits.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0:
+            return 0
+        else:
+            return int(num-9*math.floor((num-1)/9))
+        
